bff7033ce53546be78d4c0d08fc75ab11354e1b0
BeltranHervias-Italo <italo.beltranhervias@gmail.com>
2024-07-04 00:45:19 -0500

    trabajando

---
+++ script.js
@@ -0,0 +1,13 @@
+import {barcelona,roma,paris,londres} from './ciudades.js'
+
+//Obtenemos los elementos del DOM
+
+let enlaces = document.querySelectorAll('a')
+
+let tituloElemento = document.getElementById('titulo')
+let subtiuloElemento = document.getElementById('subtitulo')
+let parrafoElemento = document.getElementById('parrafo')
+
+//Agregar evento CLICK a cada enlace
+
+enlaces.forEach()
